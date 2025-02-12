09913053e70954e6abd6c30d26ac3d9bb6fb4177
Suryadas-m <suryadas4321@gmail.com>
2025-02-12 10:29:00 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class sum{
+ public static void main(String args[])
+ {
+   int a,b,sum=0;
+  Scanner obj=new Scanner(System.in);
+   
+  System.out.println("type first number:");
+  a=obj.nextInt();
+  
+  System.out.println("type second number:");
+  b=obj.nextInt();
+    
+  sum=a+b;
+  System.out.println("sum:"+sum);
+  }
+ }    
+
